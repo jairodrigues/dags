a6784b2743971e61d1b47a65772fc3950d1b40f3
Jairo Rodrigues <jairo.rjunior@telefonica.com>
2022-09-26 22:14:51 -0300

    fix:dag

--- first_pipeline.py
+++ first_pipeline.py
@@ -3,43 +3,43 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 from random import randint
-def _training_model(model):
-  return randint(1,10)
 
 def _choosing_best_model(ti):
   accuracies = ti.xcom_pull(task_ids=[
-    'training_model_A',
-    'training_model_B',
-    'training_model_C'
+  'training_model_A',
+  'training_model_B',
+  'training_model_C'
   ])
   if max(accuracies) > 8:
     return 'accurate'
   return 'inaccurate'
 
-with DAG("my_dag", start_date=datetime(2022,1,1), schedule_interval='@dayle', catchup=False) as dag:
+def _training_model(model):
+  return randint(1, 10)
+
+with DAG("my_dag",
+  start_date=datetime(2021, 1 ,1), 
+  schedule_interval='@daily', 
+  catchup=False) as dag:
   training_model_tasks = [
     PythonOperator(
       task_id=f"training_model_{model_id}",
-      python_callable= _training_model,
+      python_callable=_training_model,
       op_kwargs={
         "model": model_id
       }
     ) for model_id in ['A', 'B', 'C']
   ]
-
   choosing_best_model = BranchPythonOperator(
     task_id="choosing_best_model",
     python_callable=_choosing_best_model
   )
-
   accurate = BashOperator(
     task_id="accurate",
     bash_command="echo 'accurate'"
   )
-
-  innacurate = BashOperator(
-    task_id="innacurate",
-    bash_command="echo 'innacurate'"
+  inaccurate = BashOperator(
+    task_id="inaccurate",
+    bash_command=" echo 'inaccurate'"
   )
-
-training_model_tasks >> choosing_best_model >> [accurate, innacurate]
+training_model_tasks >> choosing_best_model >> [accurate, inaccurate]

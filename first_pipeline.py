b718b6af2dfdf35931bef2b7eaeda8f82f97dacf
Jairo Rodrigues <jairo.rjunior@telefonica.com>
2022-09-26 22:18:27 -0300

    fix:dag

--- first_pipeline.py
+++ first_pipeline.py
@@ -21,25 +21,25 @@ with DAG("my_dag",
   start_date=datetime(2021, 1 ,1), 
   schedule_interval='@daily', 
   catchup=False) as dag:
-  training_model_tasks = [
-    PythonOperator(
-      task_id=f"training_model_{model_id}",
-      python_callable=_training_model,
-      op_kwargs={
-        "model": model_id
-      }
-    ) for model_id in ['A', 'B', 'C']
-  ]
-  choosing_best_model = BranchPythonOperator(
-    task_id="choosing_best_model",
-    python_callable=_choosing_best_model
-  )
-  accurate = BashOperator(
-    task_id="accurate",
-    bash_command="echo 'accurate'"
-  )
-  inaccurate = BashOperator(
-    task_id="inaccurate",
-    bash_command=" echo 'inaccurate'"
-  )
+    training_model_tasks = [
+      PythonOperator(
+        task_id=f"training_model_{model_id}",
+        python_callable=_training_model,
+        op_kwargs={
+          "model": model_id
+        }
+      ) for model_id in ['A', 'B', 'C']
+    ]
+    choosing_best_model = BranchPythonOperator(
+      task_id="choosing_best_model",
+      python_callable=_choosing_best_model
+    )
+    accurate = BashOperator(
+      task_id="accurate",
+      bash_command="echo 'accurate'"
+    )
+    inaccurate = BashOperator(
+      task_id="inaccurate",
+      bash_command=" echo 'inaccurate'"
+    )
 training_model_tasks >> choosing_best_model >> [accurate, inaccurate]
